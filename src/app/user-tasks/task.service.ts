67ed8ca515a76eca60a708ccb53c3c82cc41f2b1
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-11-23 20:38:46 +0200

    finished up things and added saving to browser memory

--- src/app/user-tasks/task.service.ts
+++ src/app/user-tasks/task.service.ts
@@ -29,6 +29,14 @@ export class TaskService {
     },
   ];
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getTasks() {
     return this.tasks;
   }
@@ -45,9 +53,15 @@ export class TaskService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+    this.saveTasks();
   }
 
   removeTask(taskId: string) {
     this.tasks = this.tasks.filter((task) => task.id !== taskId);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
