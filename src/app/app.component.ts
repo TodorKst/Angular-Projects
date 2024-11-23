24d16083584e40581c4c516e58d058790c5c541e
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-11-23 18:30:05 +0200

    added a dynamic display of the clicked user's name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,24 @@ import { RouterOutlet } from '@angular/router';
 import {HeaderComponent} from './header/header.component';
 import {UserComponent} from './user/user.component';
 import {DUMMY_USERS} from './dummy-users';
+import {UserTasksComponent} from './user-tasks/user-tasks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserComponent],
+  imports: [HeaderComponent, UserComponent, UserTasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   users = DUMMY_USERS;
+  selectedUserId = 'u1';
+
+get selectedUser() {
+  return this.users.find(user => user.id === this.selectedUserId)!;
+}
 
   onSelectedUser(id: string) {
-    console.log(id)
+    this.selectedUserId = id;
   }
 }
---
+++ src/app/user-tasks/user-tasks.component.ts
@@ -0,0 +1,14 @@
+import {Component, Input, input} from '@angular/core';
+
+@Component({
+  selector: 'app-user-tasks',
+  templateUrl: './user-tasks.component.html',
+  standalone: true,
+  styleUrl: './user-tasks.component.css'
+})
+export class UserTasksComponent {
+  @Input( {required: true} ) name!: string;
+
+
+
+}
