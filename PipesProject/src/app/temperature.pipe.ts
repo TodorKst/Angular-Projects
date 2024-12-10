38441a4d4efc0e0c50bf3ceea0d3201fa325ba3c
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-10 13:13:45 +0200

    added a temperature pipe and a sorting pipe

---
+++ PipesProject/src/app/temperature.pipe.ts
@@ -0,0 +1,41 @@
+import {Pipe, PipeTransform} from "@angular/core";
+
+@Pipe({
+  name: 'temperature',
+  standalone: true,
+})
+export class TemperaturePipe implements PipeTransform {
+  transform(value: string | number | null, inputType: 'C' | 'F', outputType?: 'C' | 'F'): string {
+    if (!value) {
+      return '';
+    }
+    let val: number;
+    if (typeof value === 'string') {
+      val = parseFloat(value);
+    } else {
+      val = value;
+    }
+
+    let outputTemp: number;
+    if (inputType === 'C' && outputType === 'F') {
+      outputTemp = val * 9 / 5 + 32;
+    } else if (inputType === 'F' && outputType === 'C') {
+      outputTemp = (val - 32) * 5 / 9;
+    } else {
+      outputTemp = val;
+    }
+
+    let symbol: '°C' | '°F';
+
+    if (!outputType) {
+      symbol = inputType === 'C' ? '°C' : '°F';
+    } else {
+      symbol = outputType === 'C' ? '°C' : '°F';
+    }
+
+    return `${outputTemp.toFixed(2)} ${symbol}`;
+
+  }
+
+
+}
