8e4a424313ad254f7ee1bdbe81817cab2b7ce7d4
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-11 21:14:46 +0200

    cleaned up code

--- ChangeDetectionDummyApp/src/app/messages/messages-list/messages-list.component.ts
+++ ChangeDetectionDummyApp/src/app/messages/messages-list/messages-list.component.ts
@@ -1,5 +1,6 @@
 import {ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {MessagesService} from "../messages.service";
+import {AsyncPipe} from "@angular/common";
 
 @Component({
   selector: 'app-messages-list',
@@ -7,25 +8,13 @@ import {MessagesService} from "../messages.service";
   templateUrl: './messages-list.component.html',
   styleUrl: './messages-list.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    AsyncPipe
+  ]
 })
-export class MessagesListComponent implements OnInit {
-
-  ngOnInit() {
-    const subscription = this.messagesService.messages$.subscribe((messages) => {
-      this.messages = messages;
-      this.cdRef.markForCheck();
-    });
-
-    this.destroyRef.onDestroy(() => {
-      subscription.unsubscribe();
-    });
-  }
-
+export class MessagesListComponent {
   private messagesService = inject(MessagesService);
-  private cdRef = inject(ChangeDetectorRef);
-  private destroyRef = inject(DestroyRef);
-
-  messages: string[] = [];
+  messages$ = this.messagesService.messages$;
 
   get debugOutput() {
     console.log('[MessagesList] "debugOutput" binding re-evaluated.');
