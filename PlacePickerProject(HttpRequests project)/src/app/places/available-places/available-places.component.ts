c6e5794be71b0899b9e32a62a08682ad645c045f
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-12 20:48:14 +0200

    fixed up the code to actually get data to the components and added error handling

--- PlacePickerProject(HttpRequests project)/src/app/places/available-places/available-places.component.ts
+++ PlacePickerProject(HttpRequests project)/src/app/places/available-places/available-places.component.ts
@@ -4,6 +4,7 @@ import {Place} from '../place.model';
 import {PlacesComponent} from '../places.component';
 import {PlacesContainerComponent} from '../places-container/places-container.component';
 import {HttpClient} from "@angular/common/http";
+import {catchError, throwError} from "rxjs";
 
 @Component({
   selector: 'app-available-places',
@@ -14,21 +15,33 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit() {
-    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places', {
-      observe: 'response'
-    }).subscribe(({
-      next: (response) => {
-        console.log(response.body?.places);
-      }
-    }))
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(catchError((error) => {
+       return  throwError(() => new Error('Failed to fetch places'));
+      }))
+      .subscribe({
+        next: (data) => {
+          this.places.set(data.places)
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+          this.isFetching.set(false);
+        },
+        complete: () => {
+          this.isFetching.set(false);
+        }
+      });
 
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
-    })
+    });
   }
 }
