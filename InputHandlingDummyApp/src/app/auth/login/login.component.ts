a9aaa875dbb624943c088e0900fb0b53b33556dc
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-13 15:51:30 +0200

    added saving of the form on refresh

--- InputHandlingDummyApp/src/app/auth/login/login.component.ts
+++ InputHandlingDummyApp/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {afterNextRender, Component, DestroyRef, inject, viewChild} from '@angular/core';
 import {FormsModule, NgForm} from "@angular/forms";
+import {debounceTime} from "rxjs";
 
 @Component({
   selector: 'app-login',
@@ -11,6 +12,29 @@ import {FormsModule, NgForm} from "@angular/forms";
   ]
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+
+
+  constructor() {
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('loginForm');
+
+      if (savedForm) {
+        const loadedForm = JSON.parse(window.localStorage.getItem('loginForm') || '{}');
+        const savedEmail = loadedForm.email;
+        const savedPassword = loadedForm.password;
+        setTimeout(() => {
+          this.form().setValue({email: savedEmail, password: savedPassword})
+        }, 2);
+      }
+
+      this.form().valueChanges?.pipe(debounceTime(190)).subscribe({
+        next: (value) => window.localStorage.setItem('loginForm', JSON.stringify({email: value.email, password: value.password}))});
+
+    });
+  }
 
   onSubmit(formData: NgForm) {
     if (formData.invalid) {
@@ -21,7 +45,7 @@ export class LoginComponent {
     const enteredPassword = formData.value.password;
 
     formData.form.reset();
-    
+
   }
 
 }
