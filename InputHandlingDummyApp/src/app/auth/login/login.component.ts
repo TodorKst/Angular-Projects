34412b9842a3e22d35711013f605d1a74e8ea846
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-13 22:37:07 +0200

    added two ways of keeping the input after refreshing the page

--- InputHandlingDummyApp/src/app/auth/login/login.component.ts
+++ InputHandlingDummyApp/src/app/auth/login/login.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
+import {Component, DestroyRef, inject, OnInit} from '@angular/core';
 import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
-import {of} from "rxjs";
+import {debounceTime, of} from "rxjs";
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -18,6 +18,14 @@ function emailIsUnique(control: AbstractControl) {
   return of({notUnique: true});
 }
 
+const savedForm = window.localStorage.getItem('loginForm');
+let initialEmailValue = '';
+
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -25,9 +33,11 @@ function emailIsUnique(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent{
+  destroyRef = inject(DestroyRef);
+
   form = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.required, Validators.email, emailIsUnique]
     }),
     password: new FormControl('', {
@@ -35,6 +45,27 @@ export class LoginComponent {
     })
   });
 
+  // ngOnInit() {
+  //   const loginForm = window.localStorage.getItem('loginForm');
+  //
+  //   if (loginForm) {
+  //     const loadedForm = JSON.parse(loginForm);
+  //     this.form.patchValue(loadedForm);
+  //   }
+  //
+  //   const sub = this.form.valueChanges.pipe(debounceTime(150)).subscribe({
+  //       next: (value) => {
+  //         window.localStorage.setItem('loginForm', JSON.stringify({
+  //           email: value.email,
+  //         }));
+  //
+  //       }
+  //     }
+  //   )
+  //
+  //   this.destroyRef.onDestroy(() => sub.unsubscribe())
+  //
+  // }
 
   onSubmit() {
     const enteredEmail = this.form.value.email;
