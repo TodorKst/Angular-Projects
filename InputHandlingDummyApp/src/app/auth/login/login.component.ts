894aba59a501851d5803caacd15490bddab368e3
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-13 21:45:59 +0200

    reset the login component html and ts files to now apply reactive forms

--- InputHandlingDummyApp/src/app/auth/login/login.component.ts
+++ InputHandlingDummyApp/src/app/auth/login/login.component.ts
@@ -1,51 +1,9 @@
-import {afterNextRender, Component, DestroyRef, inject, viewChild} from '@angular/core';
-import {FormsModule, NgForm} from "@angular/forms";
-import {debounceTime} from "rxjs";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
-  imports: [
-    FormsModule
-  ]
 })
-export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
-
-
-
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('loginForm');
-
-      if (savedForm) {
-        const loadedForm = JSON.parse(window.localStorage.getItem('loginForm') || '{}');
-        const savedEmail = loadedForm.email;
-        const savedPassword = loadedForm.password;
-        setTimeout(() => {
-          this.form().setValue({email: savedEmail, password: savedPassword})
-        }, 2);
-      }
-
-      this.form().valueChanges?.pipe(debounceTime(190)).subscribe({
-        next: (value) => window.localStorage.setItem('loginForm', JSON.stringify({email: value.email, password: value.password}))});
-
-    });
-  }
-
-  onSubmit(formData: NgForm) {
-    if (formData.invalid) {
-      return;
-    }
-
-    const enteredEmail = formData.value.email;
-    const enteredPassword = formData.value.password;
-
-    formData.form.reset();
-
-  }
-
-}
+export class LoginComponent {}
