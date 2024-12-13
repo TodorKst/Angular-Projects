48c330a839d6a549d66c993cf60a65728a228f07
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-13 15:37:43 +0200

    added form management and error handling and form clearing

--- InputHandlingDummyApp/src/app/auth/login/login.component.ts
+++ InputHandlingDummyApp/src/app/auth/login/login.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import {FormsModule, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
+  imports: [
+    FormsModule
+  ]
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  onSubmit(formData: NgForm) {
+    if (formData.invalid) {
+      return;
+    }
+
+    const enteredEmail = formData.value.email;
+    const enteredPassword = formData.value.password;
+
+    formData.form.reset();
+    
+  }
+
+}
