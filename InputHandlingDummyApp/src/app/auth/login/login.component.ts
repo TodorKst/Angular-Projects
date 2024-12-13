ea14838f7b959631c2d0a54dc1edf6adaafab8f2
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-13 22:13:44 +0200

    added form and validation

--- InputHandlingDummyApp/src/app/auth/login/login.component.ts
+++ InputHandlingDummyApp/src/app/auth/login/login.component.ts
@@ -1,9 +1,65 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {of} from "rxjs";
+
+function mustContainQuestionMark(control: AbstractControl) {
+  if (control.value.includes('?')) {
+    return null;
+  }
+  return {doesNotContainQuestionMark: true};
+}
+
+function emailIsUnique(control: AbstractControl) {
+  console.log(control.value);
+  if (control.value !== 'test@example.com') {
+    return null;
+  }
+
+  return of({notUnique: true});
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form = new FormGroup({
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email, emailIsUnique]
+    }),
+    password: new FormControl('', {
+      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark]
+    })
+  });
+
+
+  onSubmit() {
+    const enteredEmail = this.form.value.email;
+    const enteredPassword = this.form.value.password;
+    console.log(enteredEmail);
+    console.log(enteredPassword);
+
+
+    this.form.reset();
+  }
+
+  get emailIsInvalid() {
+    return (
+      this.form.controls.email.touched &&
+      this.form.controls.email.dirty &&
+      this.form.controls.email.invalid
+    );
+  }
+
+
+  get passwordIsInvalid() {
+    return (
+      this.form.controls.password.touched &&
+      this.form.controls.password.dirty &&
+      this.form.controls.password.invalid
+    );
+  }
+}
