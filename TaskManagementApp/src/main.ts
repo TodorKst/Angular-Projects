83f33819b5400d0e640fcbdbfd2e4acbf2377d7e
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-15 00:11:03 +0200

    added basic routing to the app

--- TaskManagementApp/src/main.ts
+++ TaskManagementApp/src/main.ts
@@ -1,5 +1,7 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+import {bootstrapApplication} from '@angular/platform-browser';
 
-import { AppComponent } from './app/app.component';
+import {AppComponent} from './app/app.component';
+import {config} from "rxjs";
+import {appConfiguration} from "./app/app.config";
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfiguration).catch((err) => console.error(err));
