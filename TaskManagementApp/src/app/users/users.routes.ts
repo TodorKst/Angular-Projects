26889f919feb6eabb7356458d15bf40ed29d38ee
TodorKst <166815015+TodorKst@users.noreply.github.com>
2024-12-19 16:20:56 +0200

    added a new project focusing on deferrable views

--- TaskManagementApp/src/app/users/users.routes.ts
+++ TaskManagementApp/src/app/users/users.routes.ts
@@ -1,25 +1,30 @@
 import {Routes} from '@angular/router';
 import {canLeaveEditPage, NewTaskComponent} from '../tasks/new-task/new-task.component';
 import {resolveUserTasks, TasksComponent} from "../tasks/tasks.component";
+import {TasksService} from "../tasks/tasks.service";
 
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'tasks',
-    pathMatch: 'full',
-  },
-  {
-    path: 'tasks', // <your-domain>/users/<uid>/tasks
-    component: TasksComponent,
-    runGuardsAndResolvers: 'always',
-    resolve: {
-      userTasks: resolveUserTasks,
+    children: [{
+      path: '',
+      redirectTo: 'tasks',
+      pathMatch: 'full',
     },
-  },
-  {
-    path: 'tasks/new',
-    component: NewTaskComponent,
-    canDeactivate: [canLeaveEditPage]
+      {
+        path: 'tasks', // <your-domain>/users/<uid>/tasks
+        component: TasksComponent,
+        runGuardsAndResolvers: 'always',
+        resolve: {
+          userTasks: resolveUserTasks,
+        },
+      },
+      {
+        path: 'tasks/new',
+        component: NewTaskComponent,
+        canDeactivate: [canLeaveEditPage]
+      },],
+    providers: [TasksService]
   },
 ];
