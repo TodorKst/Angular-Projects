3c6657515921350b1bbf08c485e71db763b2f946
TodorKst <166815015+TodorKst@users.noreply.github.com>
2025-01-14 18:34:56 +0200

    made the navbar look better

--- TaskMasterApp/src/app/app.component.ts
+++ TaskMasterApp/src/app/app.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 import {HomepageComponent} from './components/homepage/homepage.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 
