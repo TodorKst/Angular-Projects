8144d9ec2b79d3ffccc370bc0b373d2b02bac23f
TodorKst <166815015+TodorKst@users.noreply.github.com>
2025-01-15 16:32:36 +0200

    added the completion/deletion of tasks

--- TaskMasterApp/src/app/components/task/task.component.ts
+++ TaskMasterApp/src/app/components/task/task.component.ts
@@ -1,10 +1,14 @@
 import {Component, Input} from '@angular/core';
 import {UserModel} from '../../models/user.model';
 import {TaskModel} from '../../models/task.model';
+import {DatePipe} from '@angular/common';
+import {TaskService} from '../../services/task/task.service';
 
 @Component({
   selector: 'app-task',
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './task.component.html',
   styleUrl: './task.component.css',
   standalone: true
@@ -13,5 +17,11 @@ export class TaskComponent {
 @Input() selectedUser: UserModel | null = null;
 @Input() selectedUserTasks: TaskModel[] = [];
 
+constructor(private taskService: TaskService) {
+}
 
+completeTask(id: number): void {
+  this.taskService.deleteTask(id);
+  this.selectedUserTasks = this.selectedUserTasks.filter(task => task.id !== id);
+}
 }
