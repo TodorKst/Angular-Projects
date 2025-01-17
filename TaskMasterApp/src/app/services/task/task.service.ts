a2d89fc69f954b2ba1505732cb2ae3e94be27178
TodorKst <166815015+TodorKst@users.noreply.github.com>
2025-01-17 19:41:08 +0200

    added saving to local browser storage

--- TaskMasterApp/src/app/services/task/task.service.ts
+++ TaskMasterApp/src/app/services/task/task.service.ts
@@ -1,17 +1,33 @@
-import { Injectable } from '@angular/core';
-import {TaskModel} from '../../models/task.model';
-import {TaskDummyData} from '../dummydata/task-dummy-data';
+import {afterNextRender, Injectable} from '@angular/core';
+import { TaskModel } from '../../models/task.model';
+import { TaskDummyData } from '../dummydata/task-dummy-data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private tasks:TaskModel[] = [];
-  private id = 13;
+  private tasks: TaskModel[] = [];
+  private id: number = 13;
+  private readonly storageKey = 'tasks';
 
   constructor() {
-    this.tasks = TaskDummyData;
+    this.loadTasks();
   }
+
+  private loadTasks() {
+    const tasks = localStorage.getItem(this.storageKey);
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    } else {
+      this.tasks = TaskDummyData;
+      this.saveTasksToStorage();
+    }
+  }
+
+  private saveTasksToStorage(): void {
+    localStorage.setItem(this.storageKey, JSON.stringify(this.tasks));
+  }
+
 
   getAllTasks(): TaskModel[] {
     return this.tasks;
@@ -26,11 +42,12 @@ export class TaskService {
       dueDate: date
     };
     this.tasks.push(task);
+    this.saveTasksToStorage(); // Save to localStorage
     console.log(task);
   }
 
   deleteTask(taskId: number): void {
     this.tasks = this.tasks.filter(task => task.id !== taskId);
+    this.saveTasksToStorage(); // Update localStorage
   }
-
 }
