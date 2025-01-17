87ad1bc00e6ece3077fc39b97e681531ca123b1b
TodorKst <166815015+TodorKst@users.noreply.github.com>
2025-01-17 19:57:57 +0200

    added validation for the creation of tasks and a default date(the current day)

--- TaskMasterApp/src/app/components/new-task/new-task.component.ts
+++ TaskMasterApp/src/app/components/new-task/new-task.component.ts
@@ -16,7 +16,8 @@ import {TaskModel} from '../../models/task.model';
 export class NewTaskComponent {
   title = '';
   description = '';
-  dueDate = '';
+  dueDate = this.getTodayDate();
+  defaultDate = this.getTodayDate();
 
   @Input() selectedUser: UserModel | null = null;
   @Input() isAddingTask!: WritableSignal<boolean>;
@@ -25,7 +26,19 @@ export class NewTaskComponent {
   constructor(private taskService: TaskService) {
   }
 
+  getTodayDate(): string {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0'); // Ensure 2-digit month
+    const day = String(today.getDate()).padStart(2, '0'); // Ensure 2-digit day
+    return `${year}-${month}-${day}`;
+  }
+
   onCreateTask() {
+    if (!this.title || !this.description || !this.dueDate) {
+      alert('Please fill in all fields.');
+      return;
+    }
     this.taskService.createTask(this.title, this.description, new Date(this.dueDate), this.selectedUser?.id || 0);
     this.isAddingTask.set(false);
     this.userTasks.set(this.taskService.getAllTasks().filter(task => task.userId === this.selectedUser?.id));
